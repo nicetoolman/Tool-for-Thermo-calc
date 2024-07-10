7add14ff17e1c3eb23c71332ab10115d21943613
ncieto_olman <137745570+nicetoolman@users.noreply.github.com>
2024-07-10 10:09:20 +0900

    Delete function_find_elements_from_filename.py

--- function_find_elements_from_filename.py
+++
@@ -1,5 +0,0 @@
-def find_elements_from_filename(filename):    
-    filename = filename.split()[0].split('-')
-    element1 = filename[0]
-    element2 = filename[1]    
-    return element1, element2
